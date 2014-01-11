622fbb01ee8fc78e1c50eaa3f57c43dfe7bb9e07
limodou <limodou@gmail.com>
2014-01-11 17:50:53 +0800

    create project

---
+++ uliweb_blogs/setup.py
@@ -0,0 +1,14 @@
+import uliweb
+from uliweb.utils.setup import setup
+import apps
+
+__doc__ = """doc"""
+
+setup(name='uliweb_blogs',
+    version=apps.__version__,
+    description="Description of your project",
+    package_dir = {'uliweb_blogs':'apps'},
+    packages = ['uliweb_blogs'],
+    include_package_data=True,
+    zip_safe=False,
+)
